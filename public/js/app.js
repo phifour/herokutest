4e2ecd19969323be959da0a8f483ffb013557bd5
Frisch <wfrisch@kpmg.com>
2016-12-09 17:08:11 +0100

    'Update'

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ui.router', 'ui.bootstrap','ngCookies','uiGmapgoogle-maps','nvd3','ngTable']);//, 
+var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'firebase', 'ngCookies','uiGmapgoogle-maps','nvd3','ngTable']);//, 
 
 app.config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
@@ -270,32 +270,40 @@ function GeoSpatialCtrl($scope, $element, $http, $interval, NgTableParams) {
 
 
 }
-app.controller('KeyMetricsCtrl', ['$scope', '$element', '$http', '$interval', 'NgTableParams', KeyMetricsCtrl]);
+app.controller('KeyMetricsCtrl', ['$scope', '$element', '$http', '$interval', 'NgTableParams', '$firebaseArray',  KeyMetricsCtrl]);
 
-function KeyMetricsCtrl($scope, $element, $http, $interval, NgTableParams) {
+function KeyMetricsCtrl($scope, $element, $http, $interval, NgTableParams, $firebaseArray) {
 
     var update_interval = 3000;
 
     var mobileView = 992;
     
-    $scope.dataupdate = false;
+    var ref = new Firebase("https://flickering-inferno-6917.firebaseio.com/temperature");
 
-    $scope.issuesclosed = 0;
-    $scope.issuesopen = 0;
+    var temp = $firebaseArray(ref);
+
+    var sin = [];
+
+     // to take an action after the data loads, use the $loaded() promise
+     temp.$loaded().then(function() {
+        console.log("loaded record:", temp);
+        loaddata();
+       // To iterate the key/value pairs of the object, use angular.forEach()
+     });
+
+    //Data is represented as an array of {x,y} pairs.
+    for (var i = 0; i < 100; i++) {
+        sin.push([i, Math.sin(i/10)]);
+    }
+
+
+    $scope.dataupdate = false;
 
     var init_load = false;
 
-    $scope.data = [
-        {
-            "key": "Quantity",
-            "bar": true,
-            "values": []
-        }
-    ];
-
     $scope.data2 = [
         {
-            "values": [],      //values - represents the array of {x,y} data points
+            "values": sin,      //values - represents the array of {x,y} data points
             key: 'Living Room Temp.', //key  - the name of the series.
             color: '#ff7f0e',  //color - optional: choose your own line color.
             strokeWidth: 3,
@@ -304,154 +312,37 @@ function KeyMetricsCtrl($scope, $element, $http, $interval, NgTableParams) {
     ];
 
     function loaddata() {
-
         $scope.dataupdate = false;
+        console.log("loaded record:", temp);
+        $scope.data2[0].values = [];
 
-        d3.csv("data/issues.csv", function (d) {
-            return {
-                customername: d['Customer name'],
-                subtimestamp: Date.parse(d['Submission Timestamp']),
-                email: d['customer email address'],
-                description: d['Description'],
-                status: d['Status'],
-                closedtimestamp: Date.parse(d['Closed Timestamp']),
-                employeename: d['Employee name']
-            };
-        }, function (data) {
-            $scope.issuesclosed = 0;
-            $scope.issuesopen = 0;
-            for (var i = 0; i < data.length; i++) {
-                if (data[i].status == "Closed") {
-                    $scope.issuesclosed = $scope.issuesclosed + 1;
-                    console.log('Issue open', $scope.issuesclosed);
-                } else {
-                    $scope.issuesopen = $scope.issuesopen + 1;
-                }
-            }
-
-            $scope.tp = new NgTableParams({}, { dataset: data });
-        });
-
-        $http.get("data/issuecount.json").then(function (response) {
-            var data = response.data;
-
-            if (data.length != $scope.data[0].values.length) {
-                $scope.dataupdate = !$scope.dataupdate;
-                $scope.data[0].values = [];               
-                // $scope.$apply;
-            }
-
-            var dummy = [];
-
-            var last = undefined;
-            for (var i = 0; i < data.length; i++) {
-                // console.log('customers',data[i]);
-                var d = Date.parse(data[i].Date);
-                // console.log(d,',',data[i].Customers);                
-                // $scope.data[0].values.push([d, data[i].Customers]);
-                last = [d, data[i].Issues];
-                dummy.push([d, data[i].Issues]);
-            }
-
-            $scope.data = [
-                {
-                    "key": "Quantity",
-                    "bar": true,
-                    "values": dummy
-                }
-            ];                
-            //bugfix
-            $scope.$apply;  
-                                            
-        })
-
-        $http.get("data/customers.json").then(function (response) {
-            var data = response.data;
-            if (data.length != $scope.data2[0].values.length) {
-                $scope.dataupdate = !$scope.dataupdate;
-                // $http.get("data/issuecount.json").then(function (res) {
-                //     return response.data;
-                // }).then(function (data) { })           
-            }
-
-            $scope.data2[0].values = [];
-
-            for (var i = 0; i < data.length; i++) {
-                // console.log('customers',data[i]);
-                var d = Date.parse(data[i].Date);
-                // console.log(d,',',data[i].Customers);
-                $scope.data2[0].values.push([d, data[i].Customers]);
-            }
-            
-            
-        });
-
-
+     for (var i=0;i<temp.length;i++){
+         //console.log(temp[i]['room temperature']);
+         console.log('date',Date.parse('2015 April 4'));
+         //$scope.data2[0].values.push([Date.parse('2015 April 4'),23]);
+         //$scope.data2[0].values.push([Date.parse('2015 April 5'),24]);
+         //$scope.data2[0].values.push([Date.parse('2015 April 6'),25]);
+         //$scope.data2[0].values.push([Date.parse('2015 April 7'),27]);
+         // var d = Date.parse('2015 April 4');
+         var temperature = temp[i]['temperature'];        
+         console.log([i, temperature]);
+         $scope.data2[0].values.push([i, temperature]);
+     }
 
     };
 
-    $scope.callAtInterval = loaddata;
+
+
+
+//    $scope.callAtInterval = loaddata;
       
     //init load of data at program start 
-    if (init_load == false) {
-        loaddata();
-        init_load = true;
-    }
+    // if (init_load == false) {
+    //     loaddata();
+    //     init_load = true;
+    // }
 
-    var stopTime = $interval(function () { $scope.callAtInterval(); }, update_interval, false);
-
-    $scope.options = {
-        chart: {
-            type: 'historicalBarChart',
-            height: 450,
-            margin: {
-                top: 20,
-                right: 20,
-                bottom: 65,
-                left: 50
-            },
-            x: function (d) { return d[0]; },
-            y: function (d) { return d[1]; },/// 100000
-            showValues: true,
-            valueFormat: function (d) {
-                return d3.format(',.1f')(d);
-            },
-            duration: 100,
-            xAxis: {
-                axisLabel: 'Time',
-                tickFormat: function (d) {
-                    return d3.time.format('%x')(new Date(d))
-                },
-                rotateLabels: 30,
-                showMaxMin: false
-            },
-            yAxis: {
-                axisLabel: 'Issues',
-                axisLabelDistance: -10,
-                tickFormat: function (d) {
-                    return d3.format(',.1f')(d);
-                }
-            },
-            tooltip: {
-                keyFormatter: function (d) {
-                    return d3.time.format('%x')(new Date(d));
-                }
-            },
-            zoom: {
-                enabled: true,
-                scaleExtent: [1, 20],
-                useFixedDomain: false,
-                useNiceScale: false,
-                horizontalOff: false,
-                verticalOff: true,
-                unzoomEventType: 'dblclick.zoom'
-            }
-        },
-        title: {
-            enable: true,
-            text: 'Number of reported issues'
-        }
-    };
+    //var stopTime = $interval(function () { $scope.callAtInterval(); }, update_interval, false);
 
     $scope.options2 = {
         chart: {
@@ -475,7 +366,9 @@ function KeyMetricsCtrl($scope, $element, $http, $interval, NgTableParams) {
             xAxis: {
                 axisLabel: 'Time',
                 tickFormat: function (d) {
-                    return d3.time.format('%x')(new Date(d))
+                    //return d3.time.format('%x')(new Date(d))
+                    return d3.format(',.1f')(d);
+
                 },
                 rotateLabels: 30,
                 showMaxMin: false
@@ -487,6 +380,8 @@ function KeyMetricsCtrl($scope, $element, $http, $interval, NgTableParams) {
                     return d3.format(',.1f')(d);
                 }
             },
+
+
             callback: function (chart) {
                 // console.log("!!! lineChart callback !!!");
             },
@@ -506,12 +401,9 @@ function KeyMetricsCtrl($scope, $element, $http, $interval, NgTableParams) {
         }
     };
 
-
-    $scope.customers = [];
-
-         $element.on('$destroy', function() {
-            $interval.cancel(stopTime);
-          });
+        //  $element.on('$destroy', function() {
+        //     $interval.cancel(stopTime);
+        //   });
 
 }
 app.controller('MainCtrl', ['$scope', '$cookieStore', MainCtrl]);
